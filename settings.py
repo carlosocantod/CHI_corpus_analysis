2414c4c8d5718cb23cf814fafbc8459fb4db7d51
Carlos Ocanto <112616839+carlosocantod@users.noreply.github.com>
2025-02-26 21:58:29 +0100

    feat: add requirements and common paths

---
+++ settings.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+PATH_DATA = Path.home() / "git_repos" / "CHI" / "data"
+PATH_RAW_CHI_METADATA = PATH_DATA / "CHI_raw.xls"
+PATH_CLEAN_CHI_METADATA = PATH_DATA / "CHI_filtered.csv"
+PATH_EMBEDDINGS = PATH_DATA / "embeddings.csv"
+COL_ABSTRACT = "Abstracts"
+COL_DOI = "DOI"
+COL_COSINE_SIMILARITY = "cosine_score"
+SBERT_MODEL_NAME = "sentence-transformers/all-MiniLM-L12-v2"
