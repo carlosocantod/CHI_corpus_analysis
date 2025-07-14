72e6fbd16c55af2b32f94af477fe32becaee53c2
Carlos Ocanto <carlos@graphmytech.fr>
2025-07-14 11:22:50 +0200

    feat: adapt home to load with cache

--- home.py
+++ home.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from settings import APP_NAME
-from setup_streamlit import load_data
+from setup_streamlit import load_data_cached
 
 
 st.set_page_config(page_title=APP_NAME, page_icon="🏗️", layout="wide")
 st.title(APP_NAME)
 st.markdown("Welcome to the CHI Papers Explorer. Use the sidebar to navigate.")
-model, embeddings, metadata, top_words_topic = load_data()
+model, embeddings, metadata, top_words_topic = load_data_cached()
