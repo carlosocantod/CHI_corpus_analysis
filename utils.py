d68af695064b63d0a3788be19882b39b1b4860d4
Carlos Ocanto <112616839+carlosocantod@users.noreply.github.com>
2025-07-14 16:08:54 +0200

    feat: add client db hybrid semantic

--- utils.py
+++ utils.py
@@ -2,12 +2,68 @@ from itertools import batched
 
 from fastembed import SparseEmbedding
 from fastembed import SparseTextEmbedding
+from fastembed import TextEmbedding
 from numpy import ndarray
 from pandera import check_types
 from pandera.typing import DataFrame
+from pydantic import BaseModel
+from qdrant_client import QdrantClient
+from qdrant_client import models
 from tqdm import tqdm
 
 from data_models import Embeddings
+from settings import SBERT_MODEL_NAME
+from settings import SPARSE_MODEL_NAME
+
+
+class HybridSearcher:
+    def __init__(self, collection_name):
+        self.collection_name = collection_name
+        self.client = QdrantClient(url="http://localhost:6333")
+        self.model_dense = TextEmbedding(model_name=SBERT_MODEL_NAME)
+        self.model_sparse = SparseTextEmbedding(model_name=SPARSE_MODEL_NAME)
+
+    def search(
+            self,
+            documents: str | list[str],
+            limit: int = 10,
+            limit_dense: int = 2_000,
+            score_threshold_dense: float = 0.7,
+            limit_sparse: int = 1_000,
+            query_filter: None | BaseModel = None,
+    ):
+        dense_embeddings = list(self.model_dense.embed(documents=documents))[0]
+        sparse_embeddings = list(self.model_sparse.embed(documents=documents))[0]
+        sparse_embeddings = models.SparseVector(
+            indices=sparse_embeddings.indices.tolist(),
+            values=sparse_embeddings.values.tolist(),
+        )
+
+        search_result = self.client.query_points(
+            collection_name=self.collection_name,
+            query=models.FusionQuery(
+                fusion=models.Fusion.RRF  # we are using reciprocal rank fusion here
+            ),
+            prefetch=[
+                models.Prefetch(
+                    query=dense_embeddings,
+                    score_threshold=score_threshold_dense,
+                    limit=limit_dense,
+                    using="dense",
+
+                ),
+                models.Prefetch(
+                    query=sparse_embeddings,
+                    limit=limit_sparse,
+                    using="sparse",
+                ),
+            ],
+            query_filter=query_filter,  # If you don't want any filters for now
+            limit=limit,  # 5 the closest results
+        ).points
+
+        return search_result
+
 
 
 @check_types()
